38b946001abba8d52cb7009687be9cf9a310ede4
jodarko <72194602+johnsondarko3@users.noreply.github.com>
2021-08-29 20:41:53 +0200

    corrected

--- Portfolio/script.js
+++
@@ -1,9 +0,0 @@
-scroll.on('scroll', (args) => {
-    // Get all current elements : args.currentElements
-    if(typeof args.currentElements['hey'] === 'object') {
-        let progress = args.currentElements['hey'].progress;
-        console.log(progress);
-        // ouput log example: 0.34
-        // gsap example : myGsapAnimation.progress(progress);
-    }
-}
